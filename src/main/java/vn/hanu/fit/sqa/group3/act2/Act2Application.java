77cb1474c6cd7afefd5f1f6a4faf1d401ec50b6c
han8129 <110092300+han8129@users.noreply.github.com>
2024-03-26 19:02:47 +0700

    Init Project

---
+++ src/main/java/vn/hanu/fit/sqa/group3/act2/Act2Application.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.sqa.group3.act2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hanu/fit/sqa/group3/act2/Act2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.sqa.group3.act2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
